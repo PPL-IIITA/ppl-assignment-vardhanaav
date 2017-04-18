09845736dea54bc835ead92742f25308bc4fce24
Ashwin Aishvarya Vardhan <iit2015104@iiita.ac.in>
2017-04-19 01:58:08 +0530

    Add files via upload

---
+++ Q11/q11.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+
+public class q11 extends Random {
+	q11() throws IOException {
+		new Couplifier();
+	}
+	public static void main(String args[]) throws IOException{
+		//Custom ob = new Custom(b, g);
+		q11 obj = new q11();
+	}
+}
+
